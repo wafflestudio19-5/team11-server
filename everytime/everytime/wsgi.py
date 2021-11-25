17dc05675eb267f71986880f760ba3908f6cbb81
esc5221 <esc5221@gmail.com>
2021-11-25 19:12:11 +0900

    refactor : project name 수정 - team11 -> everytime

--- everytime/everytime/wsgi.py
+++ everytime/everytime/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for team11 project.
+WSGI config for everytime project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'team11.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'everytime.settings')
 
 application = get_wsgi_application()
