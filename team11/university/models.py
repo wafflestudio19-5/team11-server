1b30dcadb46083220c993f42048a605fd240361e
Changhee Han <88359757+hansmaan@users.noreply.github.com>
2021-12-17 12:49:30 +0900

    Revert "Email code and other models"

--- team11/university/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-from common.models import BaseModel
-class University(BaseModel):
-    name = models.CharField(max_length=100)
-    email_domain = models.CharField(max_length=100) #서울대의 경우 snu.ac.kr을 저장
