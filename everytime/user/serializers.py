0ffaba184b4875afe5f4ac224c330a809bbecbfb
HAN CHANGHEE <gkscldfl321@naver.com>
2021-12-18 11:37:32 +0900

    user change

---
+++ everytime/user/serializers.py
@@ -0,0 +1,64 @@
+from django.contrib.auth import get_user_model, authenticate
+from django.contrib.auth.models import update_last_login
+from rest_framework import serializers
+from rest_framework_jwt.settings import api_settings
+from university.models import University
+
+# 토큰 사용을 위한 기본 세팅
+User = get_user_model()
+JWT_PAYLOAD_HANDLER = api_settings.JWT_PAYLOAD_HANDLER
+JWT_ENCODE_HANDLER = api_settings.JWT_ENCODE_HANDLER
+
+# [ user -> jwt_token ] function
+def jwt_token_of(user):
+    payload = JWT_PAYLOAD_HANDLER(user)
+    jwt_token = JWT_ENCODE_HANDLER(payload)
+    return jwt_token
+
+
+class UserCreateSerializer(serializers.Serializer):
+
+    id = serializers.CharField(required = True)
+    name = serializers.CharField(required = True)
+    email = serializers.EmailField(required=True)
+    nickname = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+    university = serializers.CharField(required = True)
+
+    def validate(self, data):
+        university_name = data.get('university')
+        try:
+            university = University.objects.get(name = university_name)
+        except University.DoesNotExist:
+            raise serializers.ValidationError("등록되지 않은 학교명입니다.")
+        return data
+
+    def create(self, validated_data):
+        email = validated_data.pop('email')
+        nickname = validated_data.pop('nickname')
+        password = validated_data.pop('password')
+        user_id = validated_data.pop('id')
+        university_name = validated_data.pop('university')
+        university = University.objects.get(name = university_name)
+        user = User.objects.create_user(user_id = user_id, nickname = nickname, email = email, password = password, university = university)
+        return user, jwt_token_of(user)
+
+class UserLoginSerializer(serializers.Serializer):
+    
+    id = serializers.CharField(max_length=64, required=True)
+    password = serializers.CharField(max_length=128, write_only=True)
+    token = serializers.CharField(max_length=255, read_only=True)
+
+    def validate(self, data):
+        user_id = data.get('id', None)
+        password = data.get('password', None)
+        user = authenticate(user_id=user_id, password=password)
+
+        if user is None:
+            raise serializers.ValidationError("이메일 또는 비밀번호가 잘못되었습니다.")
+
+        update_last_login(None, user)
+        return {
+            'id': user.user_id,
+            'token': jwt_token_of(user)
+        }
--- everytime/user/views.py
+++ everytime/user/views.py
@@ -1,26 +1,76 @@
 from rest_framework import serializers, status, viewsets, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from user.serializers import UserCreateSerializer, UserLoginSerializer
+from user.models import User
 
 # Create your views here.
 class UserSignUpView(APIView):
     permission_classes = (permissions.AllowAny, )
 
     def post(self, request, *args, **kwargs):
-        return Response({'detail': 'POST /signup/'}, status=status.HTTP_201_CREATED)
+        serializer = UserCreateSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
 
-    def get(self, request, *args, **kwargs):
-        return Response({'detail': 'GET /signup/'}, status=status.HTTP_200_OK)
+        user, jwt_token = serializer.save()
+
+        return Response({'user': user.user_id, 'token': jwt_token}, status=status.HTTP_201_CREATED)
 
 class UserLoginView(APIView):
     permission_classes = (permissions.AllowAny, )
     
     def post(self, request):
-        return Response({'detail': 'POST /login/'}, status=status.HTTP_200_OK)
+    
+        serializer = UserLoginSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        token = serializer.validated_data['token']
 
+        return Response({'success': True, 'token': token}, status=status.HTTP_200_OK)
 
 class UserViewSet(viewsets.GenericViewSet):
     permission_classes = (permissions.IsAuthenticated, )
 
     def retrieve(self, request, pk=None):
         return Response({'detail': 'GET /user/'}, status=status.HTTP_200_OK)
+
+class UserCheckIDView(APIView):
+    permission_classes = (permissions.AllowAny, )
+
+    def get(self, request, *args, **kwargs):
+        queryset = User.objects.all()
+        data = request.data
+        id = data.get('id')
+        if not id:
+            return Response({'아이디를 입력해주세요.'}, status = status.HTTP_400_BAD_REQUEST)
+        for user in queryset:
+            if user.user_id == id:
+                return Response({'중복되는 아이디입니다.'}, status = status.HTTP_409_CONFLICT)
+        return Response({'중복되지 않는 아이디입니다.'}, status = status.HTTP_200_OK)
+
+class UserCheckUsernameView(APIView):
+    permission_classes = (permissions.AllowAny, )
+
+    def get(self, request, *args, **kwargs):
+        queryset = User.objects.all()
+        data = request.data
+        nickname = data.get('nickname')
+        if not nickname:
+            return Response({'닉네임을 입력해주세요.'}, status = status.HTTP_400_BAD_REQUEST)
+        for user in queryset:
+            if user.nickname == nickname:
+                return Response({'중복되는 닉네임입니다.'}, status = status.HTTP_409_CONFLICT)
+        return Response({'중복되지 않는 닉네임입니다.'}, status = status.HTTP_200_OK)
+
+class UserCheckEmailView(APIView):
+    permission_classes = (permissions.AllowAny, )
+
+    def get(self, request, *args, **kwargs):
+        queryset = User.objects.all()
+        data = request.data
+        email = data.get('email')
+        if not email:
+            return Response({'이메일를 입력해주세요.'}, status = status.HTTP_400_BAD_REQUEST)
+        for user in queryset:
+            if user.email == email:
+                return Response({'중복되는 이메일입니다.'}, status = status.HTTP_409_CONFLICT)
+        return Response({'중복되지 않는 이메일입니다.'}, status = status.HTTP_200_OK)
