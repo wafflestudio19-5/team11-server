69a8b2f1f722e082eb5328b18f7991b769c03302
esc5221 <esc5221@gmail.com>
2021-12-18 17:24:19 +0900

    feat : error logging 추가2

--- everytime/everytime/settings.py
+++ everytime/everytime/settings.py
@@ -177,60 +177,18 @@ AUTH_USER_MODEL = 'user.User'
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse',
-        },
-        'require_debug_true': {
-            '()': 'django.utils.log.RequireDebugTrue',
-        },
-    },
-    'formatters': {
-        'django.server': {
-            '()': 'django.utils.log.ServerFormatter',
-            'format': '[%(server_time)s] %(message)s',
-        },
-        'verbose': {
-            '()': 'django.utils.log.ServerFormatter',
-            'format': '[{asctime}] {levelname} --> {message}',
-            'style': '{',
-        },
-    },
     'handlers': {
-        'console': {
-            'level': 'INFO',
-            'filters': ['require_debug_false'],
-            'class': 'logging.StreamHandler',
-        },
-        # Custom handler which we will use with logger 'django'.
-        # We want errors/warnings to be logged when DEBUG=False
-        'django.server': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'django.server',
-        },
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-        'custom_handler': {
-            'level': 'INFO',
-            'class': 'logging.handlers.TimedRotatingFileHandler',
-            'filename': f'logs/custom_handler.log',
-            'when': 'h',
-            'formatter': 'verbose',
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': '~/debug.log',
         },
     },
     'loggers': {
         'django': {
-            'handlers': ['console', 'mail_admins', 'custom_handler'],
-            'level': 'INFO',
+            'handlers': ['file'],
+            'level': 'DEBUG',
+            'propagate': True,
         },
-        'django.server': {
-            'handlers': ['django.server'],
-            'level': 'INFO',
-            'propagate': False,
-        },
-    }
+    },
 }
