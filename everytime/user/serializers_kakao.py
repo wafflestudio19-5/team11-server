418cc57e1eca75cae94c930fefef8a521b3994b2
yabsed <yabsed@snu.ac.kr>
2021-12-30 02:56:57 +0900

    Revert "change KakaoUserLoginSerializer"
    This reverts commit 4d3bdca1321b9ff0ee877c7eee1f42379b8a10a5.

--- everytime/user/serializers_kakao.py
+++ everytime/user/serializers_kakao.py
@@ -3,7 +3,7 @@ from university.models import University
 from allauth.socialaccount.models import SocialAccount
 from requests import get
 from .models import User
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from django.contrib.auth.models import update_last_login
 from django.db import IntegrityError
 from rest_framework_jwt.settings import api_settings
@@ -81,10 +81,10 @@ class KakaoUserLoginSerializer(serializers.Serializer):
         kakao_id = user_info_response.json().get('id')
 
         if kakao_id is None:
-            raise serializers.ValidationError
+            raise serializers.ValidationError("카카오 계정이 존재하지 않습니다")
 
         if not SocialAccount.objects.filter(uid=kakao_id, provider="kakao"):
-            raise FileNotFoundError
+            raise serializers.ValidationError("카카오 계정이 존재하지 않습니다")
 
         socialAccount = SocialAccount.objects.get(uid=kakao_id, provider="kakao")
         user = socialAccount.user
