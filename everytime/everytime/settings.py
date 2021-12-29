1bb67f1ec43a6aa74a91fbc5e7342f4febd39a1f
yabsed <yabsed@snu.ac.kr>
2021-12-30 04:15:27 +0900

    email security

--- everytime/everytime/settings.py
+++ everytime/everytime/settings.py
@@ -106,13 +106,20 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 20
 }
 
+try:
+    f = open("everytime/account.txt", 'r')
+    google_account = f.read().split("\n")
+    f.close()
+except:
+    pass
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "waffle.team11@gmail.com"
-EMAIL_HOST_PASSWORD = "showmethemoney"
-DEFAULT_FROM_MAIL = "waffle.team11"
+EMAIL_HOST_USER = google_account[0]+"@gmail.com"
+EMAIL_HOST_PASSWORD = google_account[1]
+DEFAULT_FROM_MAIL = "waffle"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
