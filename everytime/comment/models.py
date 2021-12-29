4df02e3ab36d46527120e361597463a0545c1daf
esc5221 <esc5221@gmail.com>
2021-12-29 22:40:31 +0900

    feat : comment_create : - comment 모델 생성 - comment_create만 구현

--- everytime/comment/models.py
+++ everytime/comment/models.py
@@ -4,18 +4,16 @@ from user.models import User
 from common.models import BaseModel
 
 class Comment(BaseModel):
-    
-    pass
-    #disable for partial deploy
-    # article = models.ForeignKey(Article, on_delete=models.CASCADE) 
-    # parent = models.ForeignKey('self', on_delete=models.PROTECT, null=True)
-    # text = models.CharField(max_length=300, null = False)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    # commenter = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='comments', null = True)
-    # like_count = models.PositiveIntegerField(default=0)
-    # is_writer = models.BooleanField(default=False, null = False)
-    # is_anonymous = models.BooleanField(default=True, null = False)
-    # is_active = models.BooleanField(default=True, null = False)
+
+    article = models.ForeignKey(Article, on_delete=models.CASCADE) 
+    parent = models.ForeignKey('self', on_delete=models.PROTECT, null=True)
+    text = models.CharField(max_length=300, null = False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    commenter = models.ForeignKey(User, on_delete=models.SET_NULL, related_name='comments', null = True)
+    like_count = models.PositiveIntegerField(default=0)
+    is_writer = models.BooleanField(default=False, null = False)
+    is_anonymous = models.BooleanField(default=True, null = False)
+    is_active = models.BooleanField(default=True, null = False)
 
     # Serializer field only
     #is_mine True,
--- everytime/comment/serializers.py
+++ everytime/comment/serializers.py
@@ -1,24 +1,39 @@
-from django.core.exceptions import ObjectDoesNotExist
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from rest_framework import serializers, status
 from comment.models import Comment
+from article.models import Article
 
 class CommentCreateSerializer(serializers.ModelSerializer):
 
-    title = serializers.CharField(required = True)
+    parent = serializers.CharField(allow_blank=True)
     text = serializers.CharField(required = True)
-    is_anonymous = serializers.BooleanField(required=True)
-    is_question = serializers.BooleanField(required=True)
-    board = serializers.IntegerField()
-    
+    is_anonymous = serializers.BooleanField()
     class Meta:
         model =  Comment
-        fields = '__all__'
+        fields = ('parent','text','is_anonymous')
 
     def validate(self, data):
+        parent=data.get('parent')
+        if parent in ("", 0):
+            parent = None
+            data.pop('parent')
+
+        if 'parent' in data and Comment.objects.get_or_none(id=parent)==None:
+            raise ValidationError(({"detail": "원댓글이 존재하지 않습니다."}))
         
         return data
 
     def create(self, validated_data):
+        if 'parent' in validated_data:
+            validated_data['parent'] = Comment.objects.get(id=validated_data['parent'])
+
+        article = Article.objects.get_or_none(id=self.context['article_id'])
+        validated_data['article'] = article
+        validated_data['is_writer'] = (
+            self.context['request'].user == article.writer
+        )
+        validated_data['commenter'] = self.context['request'].user
+
         comment = Comment.objects.create(**validated_data)
         return comment
 
