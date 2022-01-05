2b50ef3dc3f3bd09a80bd3515f3cdebba2f2a345
HAN CHANGHEE <gkscldfl321@naver.com>
2022-01-05 20:02:03 +0900

    article image create

--- everytime/article/models.py
+++ everytime/article/models.py
@@ -18,3 +18,8 @@ class UserArticle(BaseModel):
     article = models.ForeignKey(Article, on_delete=models.SET_NULL, related_name='user_article', null = True)
     like = models.BooleanField(default = False, null = False)
     scrap = models.BooleanField(default = False, null = False)
+
+class ImageArticle(BaseModel):
+    image = models.ImageField(null = True)
+    description = models.CharField(max_length=5000, null = True)
+    article = models.ForeignKey(Article, on_delete=models.SET_NULL, related_name='image_article', null = True)
