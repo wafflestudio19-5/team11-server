f4c4d82034c888267a70b6f8b521c589127298eb
EC2 Default User <ec2-user@ip-172-31-2-64.us-east-2.compute.internal>
2022-01-06 14:09:52 +0000

    fix : ImageArticle image필드 변경

--- everytime/article/models.py
+++ everytime/article/models.py
@@ -4,6 +4,8 @@ from board.models import Board
 from user.models import User
 from common.models import BaseModel
 
+from user.utils import upload_image
+
 class Article(BaseModel):
     title = models.CharField(max_length=100, null = False)
     board = models.ForeignKey(Board, on_delete=models.CASCADE, related_name='articles', null = True)
@@ -20,6 +22,6 @@ class UserArticle(BaseModel):
     scrap = models.BooleanField(default = False, null = False)
 
 class ImageArticle(BaseModel):
-    image = models.ImageField(null = True)
+    image = models.ImageField(upload_to = upload_image, editable = False, null = False)
     description = models.CharField(max_length=5000, null = True)
     article = models.ForeignKey(Article, on_delete=models.SET_NULL, related_name='image_article', null = True)
