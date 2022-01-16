e0104feee2b883e4f4a14764c389b8406ca9171d
yabsed <yabsed@snu.ac.kr>
2022-01-17 03:25:46 +0900

    같은 강의 -> 시험에 따라 다른 강의평을 작성할 수 있음

--- everytime/information/serializers.py
+++ everytime/information/serializers.py
@@ -16,7 +16,7 @@ class InformationCreateSerializer(serializers.ModelSerializer):
     year = serializers.IntegerField()
     season = serializers.IntegerField()
     test_number = serializers.IntegerField()  # 기타는 0번
-    test_method = serializers.IntegerField()
+    test_method = serializers.CharField(max_length=150)
     strategy = serializers.CharField(max_length=1000)
     problems = serializers.CharField(max_length=1000)
 
@@ -27,7 +27,7 @@ class InformationCreateSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         user = self.context['request'].user
-        year, season= data['year'], data['season']
+        year, season, test_number= data['year'], data['season'], data['test_number']
 
         if not (subject_professor := SubjectProfessor.objects.get_or_none(id=data['subject_professor'])):
             raise CustomException("존재하지 않는 SubjectProfessor 입니다.", status.HTTP_404_NOT_FOUND)
@@ -35,7 +35,7 @@ class InformationCreateSerializer(serializers.ModelSerializer):
             raise CustomException("Professor가 NULL인 SubjectProfessor입니다. ", status.HTTP_400_BAD_REQUEST)
         if not Lecture.objects.filter(subject_professor=subject_professor, year=year, season=season):
             raise CustomException("해당 학기에 강의가 없습니다. ", status.HTTP_404_NOT_FOUND)
-        if Information.objects.filter(user=user, year=year, season=season):
+        if Information.objects.filter(user=user, year=year, season=season, test_number=test_number):
             raise CustomException("이미 시험정보를 작성한 적이 있습니다.", status.HTTP_409_CONFLICT)
 
         return data
@@ -68,5 +68,9 @@ class InformationViewSerializer(serializers.ModelSerializer):
         return Lecture.SeasonCode[obj.season]
 
     def get_test_method(self, obj):
-        return Information.TestMethodCode[obj.test_method]
+        data = ""
+        for i in obj.test_method.split(' '):
+            data += (Information.TestMethodCode[int(i)]) + ", "
+        data = data[:-2]
+        return data
 
